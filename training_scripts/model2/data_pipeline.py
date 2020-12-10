75fbcbbf153af75e1984d7e2ac1e4f36c4abceb9
Darshan Solanki <56557988+Darshansol9@users.noreply.github.com>
2020-12-10 01:23:40 -0500

    made oops files

--- training_scripts/model2/data_pipeline.py
+++ training_scripts/model2/data_pipeline.py
@@ -1,23 +1,76 @@
-import pickle
+from pickle5 import pickle
 import os
+from gensim.models import Word2Vec
+import numpy as np
+import nltk
+nltk.download('punkt')
 
-def read_data(dir_to_look,call_func):
+
+
+def generateEmbeddings(sequence,vocab_size,wordtoidx,max_length,embedding_dim):
+
+  w2v = Word2Vec(sequence, size=embedding_dim, window=5, min_count=1, workers=4)
+  embedding_matrix = np.zeros((vocab_size, embedding_dim))
+  for word, i in wordtoidx.items():
+      embedding_vector = w2v.wv[word]
+      embedding_matrix[i] = embedding_vector
+
+  return embedding_matrix
+
+
+def convert_to_set(data,delimiter):
+
+  unique_names = set()
+  for row in data:
+    row = row.split(delimiter)[0]
+    fname,fext = os.path.splitext(row)
+    unique_names.add(fname)
+
+  return unique_names
+
 
   
-  f = open(os.path.join(dir_to_look,'dict_text.pickle'),'rb')
-      sequence = pickle.load(f)
+def train_val_split(dict_encoding,text_sequence,ignore_file_name,val_file_name,train_file_name):
 
-  with open(os.path.join(dir_to_look,'corpus_text.txt'),'r') as f:
-    vocab_lst = f.readlines()
+  ignore_names = convert_to_set(ignore_file_name,'#')
+  val_names = convert_to_set(val_file_name,'#')
+  train_names = convert_to_set(train_file_name,'#')
+  dict_text_to_name = {k.split('_')[1]:k for k,v in text_sequence.items()}
+  dict_encoding_train = {}
+  dict_encoding_val = {}
+  seq_train = {}
+  seq_val = {}
 
-  if(call_func == 'word_embedding'):
-      return sequence,vocab_lst
+  for k,v in dict_encoding.items():
+    fname = k.split('_')[1]
+    if(fname not in ignore_names):
+      if(fname in train_names):
+        dict_encoding_train[k] = v
+        seq_train[dict_text_to_name[fname]] = text_sequence[dict_text_to_name[fname]]
+      else:
+        dict_encoding_val[k] = v
+        seq_val[dict_text_to_name[fname]] = text_sequence[dict_text_to_name[fname]]
+
+  return dict_encoding_train,seq_train,dict_encoding_val,seq_val,dict_text_to_name
+
+
+def read_pickle(dir_to_look,name):
+
+  f = open(os.path.join(dir_to_look,name),'rb')
+  return pickle.load(f)
+
+
+def read_text(dir_to_look,name=None):
+
+  if(name is not None):
+    with open(os.path.join(dir_to_look,name),'r') as f:
+      data = f.readlines()
 
   else:
-    e = open(os.path.join(dir_to_look,'dict_encodings.pickle'),'rb')
-    dict_encoding = pickle.load(e)
+    with open(dir_to_look,'r') as f:
+      data = f.readlines()
 
-    return dict_encoding,sequence,vocab_lst
+  return data
 
 
 def compute_vocab(vocab_lst):
--- training_scripts/model2/image_encodings.py
+++ training_scripts/model2/image_encodings.py
@@ -1,18 +1,18 @@
-##%tensorflow_version 2.x
+####%tensorflow_version 2.x
 ##import tensorflow as tf
 ##device_name = tf.test.gpu_device_name()
 ##if device_name != '/device:GPU:0':
 ##  raise SystemError('GPU device not found')
 ##print('Found GPU at: {}'.format(device_name))
 
-from read_alignments import read_align
+from read_alignment import read_align
 from video_crop import read_video
 import numpy as np
 from numpy import array
 import os
 import math
-from keras.applications.vgg19 import VGG19
-from keras.applications.vgg19 import preprocess_input
+from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.applications.vgg19 import preprocess_input
 import pickle
 
 
@@ -23,63 +23,163 @@ class Encodings:
     def __init__(self):
 
         self.model_vgg = VGG19(weights='imagenet', include_top=False)
-        self.num_samples = 1
+        self.num_samples = 5
         self.dict_encodings = {}
         self.vocab_set = set()
         self.dict_text = {}
+        self.max_len = -float('inf')
+        self.ignore_video_files = set()
+
+    def pad_images_to_maxlength(self):
+
+        vgg_shape = self.dict_encodings[list(self.dict_encodings.keys())[0]].shape[1:]
+        for k,v in self.dict_encodings.items():
+            result = np.zeros((self.max_len,vgg_shape[0],vgg_shape[1],vgg_shape[2]))
+            result[:v.shape[0], :v.shape[1], :v.shape[2], :v.shape[3]] = v
+            self.dict_encodings[k] = result.flatten()
+
+        path_to_save = '/scratch/vvt223/data/processed_data'
+        
+        with open(os.path.join(f'{path_to_save}','flatten_encodings.pickle'),'wb') as f:
+            pickle.dump(self.dict_encodings, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        
+    def generateEncodingSamples(self,data,mode):
 
 
-    def generateEncodings(self,dir_path,mode):
-
-      for folder in os.listdir(dir_path):
-
-        s_path = os.path.join(dir_path,folder)
-        #print(s_path)
-
+        error_file = open('/scratch/vvt223/data/processed_data/error_train_encoding_file.txt','w')
         count_samples = 0
-
-        for files in os.listdir(s_path):
-
-          fname,fext = os.path.splitext(files)
-          alignments = read_align(os.path.join(os.path.join(s_path,'align'),fname+'.align'))
-          mouth_video = read_video(os.path.join(s_path,fname+'.mpg'))
+        
+        
+        for paths in data:
+          paths = paths.replace('\n','')
+          read_align_path,video_align_path = paths.split('#')[0],paths.split('#')[1]
+          folder,file = paths.split('/')[5],paths.split('/')[-1]
+          fname,fext = os.path.splitext(file)
+          
+          if(fext == '.db'):
+              continue
+             
+          alignments = read_align(read_align_path)
+          mouth_video = read_video(video_align_path)
         
           word_count = 0
+          first_occurence = False
           sentence = ['start']
           for start, stop, word in alignments:
-              if word == 'sil' or word == 'sp':
-                continue
-                                          
-              if start < stop and stop < len(mouth_video):
-                  img_frames = mouth_video[int(math.floor(start)):int(math.floor(stop))]
-                  img_preprocess_frames = preprocess_input(img_frames)
-                  img_features = self.model_vgg.predict(img_preprocess_frames)
-                  self.dict_encodings[f'{folder}_{fname}_word{word_count}_{mode}'] = img_features
-                  word_count +=1
-                  if(mode == 'train'):
-                    self.vocab_set.add(word)
-              else:
-                continue
+              if word == 'sil' and not first_occurence:
+                  first_occurence = True
+                  continue
+
+              try:              
+                  if start < stop and stop < len(mouth_video):
+                      img_frames = mouth_video[int(math.floor(start)):int(math.floor(stop))]
+                      img_preprocess_frames = preprocess_input(img_frames)
+                      img_features = self.model_vgg.predict(img_preprocess_frames)
+                      self.dict_encodings[f'{folder}_{fname}_word{word_count}_{mode}'] = img_features
+                      word_count +=1
+                      if(mode == 'train' and word != 'sil'):
+                          self.max_len = max(self.max_len,int(math.floor(stop))-int(math.floor(start)))
+                          self.vocab_set.add(word)
+                      if(word != 'sil'):
+                          sentence.append(word)
+                  else:
+                    continue
+              except Exception as e:
+                  print('-'*40)
+                  print(f'Cannot processed {folder}_{fname}_word{word_count}')
+                  error_file.write(f'{folder}_{fname}_word{word_count}')
+                  self.ignore_video_files.add(fname)
+                  error_file.write('\n')
+                  print('-'*40)
+                  break
+                
 
           sentence.append('end')
-          self.dict_text[f'{folder}_{fname}_{mode}'] = sentence[::]
-          
-          if(count_samples == self.num_samples):
-            break
+          if(not fname in self.ignore_video_files):
+              self.dict_text[f'{folder}_{fname}_{mode}'] = sentence[::]
 
-          print(f'Sample {count_samples} finished for speaker {folder}')
+          print(f'Sample {count_samples} finished')
           count_samples +=1
+          
+          #if(count_samples == self.num_samples):
+          #    print('Breaking')
+          #    break
+
+        error_file.close()
+        
+
+##    def generateEncodings(self,dir_path,mode):
+##
+##      error_file = open('/scratch/vvt223/data_processed/error.file.txt','wb')
+##      
+##      for folder in os.listdir(dir_path):
+##
+##        s_path = os.path.join(dir_path,folder)
+##        #print(s_path)
+##
+##        count_samples = 0
+##
+##        for files in os.listdir(s_path+'/video'):
+##
+##          fname,fext = os.path.splitext(files)
+##          if(fext == '.db'):
+##              continue
+##             
+##          alignments = read_align(os.path.join(os.path.join(s_path,'align'),fname+'.align'))
+##          mouth_video = read_video(os.path.join(s_path+'/video',fname+'.mpg'))
+##        
+##          word_count = 0
+##          first_occurence = False
+##          sentence = ['start']
+##          for start, stop, word in alignments:
+##              if word == 'sil' and not first_occurence:
+##                  first_occurence = True
+##                  continue
+##
+##              try:              
+##                  if start < stop and stop < len(mouth_video):
+##                      img_frames = mouth_video[int(math.floor(start)):int(math.floor(stop))]
+##                      img_preprocess_frames = preprocess_input(img_frames)
+##                      img_features = self.model_vgg.predict(img_preprocess_frames)
+##                      self.dict_encodings[f'{folder}_{fname}_word{word_count}_{mode}'] = img_features
+##                      word_count +=1
+##                      if(mode == 'train' and word != 'sil'):
+##                        self.vocab_set.add(word)
+##                      if(not first_occurence):
+##                          sentence.append(word)
+##                  else:
+##                    continue
+##              except Exception as e:
+##                  print('-'*40)
+##                  print(f'Cannot processed {folder}_{fname}_word{word_count}')
+##                  error_file.write(f'{folder}_{fname}_word{word_count}')
+##                  error_file.write('\n')
+##                  print('-'*40)
+##                
+##
+##          sentence.append('end')
+##          self.dict_text[f'{folder}_{fname}_{mode}'] = sentence[::]
+##          
+##          if(count_samples == self.num_samples):
+##            break
+##
+##          print(f'Sample {count_samples} finished for speaker {folder}')
+##          count_samples +=1
+##
+##      error_file.close()  
 
 
     def saveEncodings(self):
 
-        path_to_save = '/content/data_processed/'
+        path_to_save = '/scratch/vvt223/data/processed_data'
         
         with open(os.path.join(f'{path_to_save}','image_encodings.pickle'),'wb') as f:
             pickle.dump(self.dict_encodings, f, protocol=pickle.HIGHEST_PROTOCOL)
 
-        with open(os.path.join(f'{path_to_save}','text_encodings.pickle'),'wb') as e:
+        with open(os.path.join(f'{path_to_save}','text_sentences.pickle'),'wb') as e:
             pickle.dump(self.dict_text, e, protocol=pickle.HIGHEST_PROTOCOL)
+
 
         vocabulary = ''
         vocabulary += "\n".join((str(v) for v in self.vocab_set))
@@ -87,11 +187,38 @@ class Encodings:
             f.write(vocabulary)
         f.close()
 
+        ignore_videos = ''
+        ignore_videos += "\n".join((str(v) for v in self.ignore_video_files))
+        with open(os.path.join(f'{path_to_save}','ignore_videos_names.txt'),'w') as f:
+            f.write(ignore_videos)
+        f.close()
+
   
 
 if __name__ == '__main__':
                   
     encod = Encodings()
-    encod.generateEncodings('/content/seen_speakers/',mode='train')
-    encod.generateEncodings('/content/unseen_speakers/',mode='test')
+    with open('/scratch/vvt223/data/X_train_10000.txt','r') as f:
+        data_train = f.readlines()
+    f.close()
+
+    with open('/scratch/vvt223/data/X_val_3000.txt','r') as f:
+        data_val = f.readlines()
+    f.close()
+    with open('/scratch/vvt223/data/X_test_2000.txt','r') as f:
+        data_test = f.readlines()
+    f.close()
+
+    print('------------Train Routine Started-------------------')
+    encod.generateEncodingSamples(data_train[0:8000],'train')
+    print('------------Validation Routine Started-------------------')
+    encod.generateEncodingSamples(data_val[0:2000],'train')
+    print('------------Test Routine Started-------------------')
+    encod.generateEncodingSamples(data_test[0:1500],'test')
+    #encod.generateEncodings('/scratch/vvt223/data/seen_speakers/',mode='train')
+    #encod.generateEncodings('/scratch/vvt223/data/unseen_speakers/',mode='test')
+    print('------------Encodings Saved-------------------')
     encod.saveEncodings()
+    encod.pad_images_to_maxlength()
+    print('------------Padded to max_length-------------------')
+    print('Completed')
